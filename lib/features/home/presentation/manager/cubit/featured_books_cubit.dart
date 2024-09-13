5e155ef0aaaf32d37f2fe5dd444828df5cf2f619
wisam4tech <135574937+wisam4tech@users.noreply.github.com>
2024-09-13 16:21:08 +0300

    create featured books method

---
+++ lib/features/home/presentation/manager/cubit/featured_books_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'featured_books_state.dart';
+
+class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+  final HomeRepo homeRepo;
+  BookModel? bookModel;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failure) {
+        emit(FeaturedBooksFailure(failure.errMessage));
+      },
+      (books) {
+        FeaturedBooksSuccess(books);
+      },
+    );
+  }
+}
--- lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
-import 'package:equatable/equatable.dart';
-
-part 'featured_books_state.dart';
-
-class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
-}
